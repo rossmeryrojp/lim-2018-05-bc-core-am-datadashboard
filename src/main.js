fb6d3fedb011cbe097d2423d816f490bbea72028
Ailim <ailim.moscoso@gmail.com>
2018-06-21 23:04:07 -0500

    agrega info de usuaria

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 const databutton = document.getElementById('cohort'); //traigo al boton
 const select = document.getElementById('selector');
 const lista = document.getElementById('lista');
-const pruebaBtn = document.getElementById('prueba-btn')
+const pruebaBtn = document.getElementById('prueba-btn');
 
+let progressUsers = [];
 
 databutton.addEventListener('click', function (e) { //evento del boton para get list
   e.preventDefault(); //para que no recargue
@@ -23,11 +24,25 @@ function addCohorts(event) {
     option.innerHTML = cohort.id; //las inserto en el html
     option.value = cohort.id; //inserta el id de esa data
     select.appendChild(option); //agrego todas las opciones
-
   });
 }
 
-function getUsers(parametro) { //le pongo un parametro que es el selector
+// hago el evento al selector "change", cuando escoja realiza la funcionn de la lista de users, tiene q tener parametro
+select.addEventListener('change', () => { //al selector se le adiciona el evento de escoger
+  getProgress();
+  getUsers(select.value);//ejecuta la funcion del user con el parametro
+});
+
+function getProgress () {
+  const getProgressUsers = new XMLHttpRequest();
+  getProgressUsers.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/progress.json');
+  getProgressUsers.onload = function (event) {
+    progressUsers = JSON.parse(event.target.responseText);
+  }
+  getProgressUsers.send();
+}
+
+function getUsers (cohort) { //le pongo un parametro que es el selector
   const getName = new XMLHttpRequest(); //creo una nueva instancia
   getName.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json'); //le digo q llame al url
   getName.onload = addUsers; //agrego la funcion para mostrar la lista de users
@@ -35,17 +50,19 @@ function getUsers(parametro) { //le pongo un parametro que es el selector
     const names = JSON.parse(this.responseText); // me la va formatear en json
     let array=[]; //creo array vacio para almacenar los users correspondientes al cohort
     names.forEach(users => { // recorre la respuesta del cohort por cada elemento
-      if (parametro === users.signupCohort) {//cuando comparo el valor del selector con el names.signup
+      if (cohort === users.signupCohort) {//cuando comparo el valor del selector con el names.signup
         array.push(users); //cuando cumple esa funcion, en el array vacio adiciona los elementos
         let li = document.createElement('li'); //almaceno en una variable las opciones q voy a crear
-        li.innerHTML = users.name; //las inserto en el html
+        let a = document.createElement('a');
+        a.innerHTML = users.name;
+        a.addEventListener("click", function () {
+          document.getElementById("user").innerHTML = JSON.stringify(users);
+          document.getElementById("progress").innerHTML = JSON.stringify(progressUsers[users.id]);
+        });
+        li.appendChild(a); //las inserto en el html
         lista.appendChild(li); //agrego todas las opciones
       }
     });
   }
   getName.send();//si no se pone, no envia nada
 }
-// hago el evento al selector "change", cuando escoja realiza la funcionn de la lista de users, tiene q tener parametro
-select.addEventListener('change', ()=>{ //al selector se le adiciona el evento de escoger
-    getUsers(select.value)//ejecuta la funcion del user con el parametro
-} );
